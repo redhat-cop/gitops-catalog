e0adf13d2682bcce147ebecaf10070df30e80821
Thomas Jungbauer <tjungbau@redhat.com>
2021-11-01 19:55:39 +0100

    added mode 700 for folder ./bitnami to mkdir inside get-sealed-secret-key.sh

--- sealed-secrets-operator/scripts/get-sealed-secret-key.sh
+++ sealed-secrets-operator/scripts/get-sealed-secret-key.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Getting public key from Sealed Secrets secret and copying it to ~/.bitnami"
 echo "Create dir for Sealed Secrets public key. (~/.bitnami)."
-mkdir -p ~/.bitnami
+mkdir -m 700 -p ~/.bitnami
 echo "Backup secret itself"
 oc get $(oc get secret -n sealed-secrets -l sealedsecrets.bitnami.com/sealed-secrets-key=active -o name) -n sealed-secrets -o yaml >  ~/.bitnami/sealed-secrets-secret.yaml
 echo "Get the public key from the Sealed Secrets secret."
